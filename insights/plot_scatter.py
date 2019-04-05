df6948bfe600994d943fd7db85be31310c5e95df
Gsllchb <gsllchb@gmail.com>
2019-04-05 15:11:15 +0800

    create plot_scatter.py

---
+++ insights/plot_scatter.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+import random
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import dotdot
+from util import *
+
+
+def main():
+    data = pd.read_csv("../data/signal_ana_20190221_105MeV.zip")
+    data = data[data["Row"] < 100]
+    first_turns = data[data["MC_hit_tu"] == 1]
+    other_turns = data[data["MC_hit_tu"] != 1]
+    first_turns_label = "first turn"
+    other_turns_label = "other turn"
+
+    plt.scatter(
+        first_turns["DT_drift"],
+        np.log2(first_turns["MC_hit_ed"]),
+        label=first_turns_label,
+        s=1,
+        alpha=0.5,
+    )
+    plt.scatter(
+        other_turns["DT_drift"],
+        np.log2(other_turns["MC_hit_ed"]),
+        label=other_turns_label,
+        s=1,
+        alpha=0.5,
+    )
+    plt.legend()
+    plt.xlabel("drift time")
+    plt.ylabel("deposit energy (log2 scale)")
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
