1a2361b870e9ee1fba5d0d546f29db10e668589e
Gsllchb <gsllchb@gmail.com>
2019-04-10 11:34:12 +0800

    improve pref

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 # coding: utf-8
 """Test the overall metrics of final models in hit level"""
-import dot
+from sklearn.metrics import log_loss, auc, roc_curve
 
-from sklearn.metrics import log_loss, roc_auc_score, roc_curve
-
-from util import *
 from layerly_train import MIN_SAMPLES_LEAVES
+from util import *
 
 MIN_TPR = 0.99
 
@@ -19,7 +17,7 @@ def main():
     y_test = flatten(Y_test)
 
     sub_preds = []
-    for layer, clf in enumerate(clfs):
+    for clf in clfs:
         sub_preds.append(clf.predict_proba(X_test))
     Y_pred = np.hstack(sub_preds)
     y_pred = flatten(Y_pred)
@@ -27,19 +25,19 @@ def main():
     logloss = log_loss(y_test, y_pred)
     print("logloss: {}".format(logloss))
 
-    auc = roc_auc_score(y_test, y_pred)
-    print("auc: {}".format(auc))
+    fprs, tprs, _ = roc_curve(y_test, y_pred)
+    auc_score = auc(fprs, tprs)
+    print("auc: {}".format(auc_score))
 
     n_null_hits = np.sum(X_test == X_test.max()) // 2
     null_hit_rate = n_null_hits / np.sum(Y_test == 0)
     # null_hit_rate = 0
 
-    fprs, tprs, _ = roc_curve(y_test, y_pred)
     for fpr, tpr in zip(fprs, tprs):
         if tpr >= MIN_TPR:
             acceptance = tpr
             rejection = ((1 - fpr) - null_hit_rate) / (1 - null_hit_rate)
-            msg = "first turn acceptance: {}, other turn rejection: {}."
+            msg = "first turn hit acceptance: {}, other turn hit rejection: {}."
             print(msg.format(acceptance, rejection))
             break
 
